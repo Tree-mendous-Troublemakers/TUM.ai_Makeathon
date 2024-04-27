2c93559eabe01cb55c571b0c647f52d0633eb3db
Julia Walczak <juliawalczak@yahoo.de>
2024-04-27 11:53:45 +0200

    test for image loading

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,9 +1,31 @@
+from libtiff import TIFF # an alternative 
+
+
+#im = Image.open('test.tif')
+#im.show()
+
+
+
+tif = TIFF.open('test.tif') # open tiff file in read mode
+# read an image in the current TIFF directory as a numpy array
+image = tif.read_image()
+
+# read all images in a TIFF file:
+for image in tif.iter_images(): 
+    print("Hi")
+    pass
+
+tif = TIFF.open('test.tif', mode='w')
+tif.write_image(image)
+
+
 
 
 def get_data():
     pass
 
 def preprocess(data): 
+    # resizing or normalization 
     pass
 
 
